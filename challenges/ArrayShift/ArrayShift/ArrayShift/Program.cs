4ced4ea261e38cb2655d754cf9be3ae5c4f50b93
Andrew <shifted6@gmail.com>
2020-03-18 00:38:36 -0700

    ShiftArray now creates new array and populates it with values from the old array depending on index.

---
+++ challenges/ArrayShift/ArrayShift/ArrayShift/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static int[] InsertShiftArray(int[] array, int value)
+        {
+            int[] shiftedArray = new int[array.Length + 1]; // Creates the new, shifted array where we will be adding our value
+            int middleIndex = (array.Length+1) / 2;
+            for(int i=0; i < shiftedArray.Length; i++)
+            {
+                if (i == middleIndex)
+                {
+                    shiftedArray[i] = value;
+                } else if(i < middleIndex)
+                {
+                    shiftedArray[i] = array[i];
+                } else if(i > middleIndex)
+                {
+                    shiftedArray[i] = array[i - 1];
+                }
+            }
+            return shiftedArray;
+        }
+    }
+}
