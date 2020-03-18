2b7aa0a71c561053e0f7d6f3bd6b22d7a4247e57
Andrew <shifted6@gmail.com>
2020-03-18 12:39:57 -0700

    Added simple testing of ArrayShift and images of application working. Created test file but learned it is not required. Updated readme.

--- challenges/ArrayShift/ArrayShift/ArrayShift/Program.cs
+++ challenges/ArrayShift/ArrayShift/ArrayShift/Program.cs
@@ -6,13 +6,35 @@ namespace ArrayShift
     {
         static void Main(string[] args)
         {
-            
+            int value = 7;
+            int[] oddArray = { 2, 4, 6, 8, 10 };
+            int[] shiftedArray = ShiftArray(oddArray, value);
+            foreach (int number in shiftedArray)
+            {
+                Console.Write(number);
+            }
+
+            value = 5;
+            int[] evenArray = { 2, 4, 6, 8 };
+            shiftedArray = ShiftArray(evenArray, value);
+            Console.WriteLine();
+            foreach(int number in shiftedArray)
+            {
+                Console.Write(number);
+            }
         }
-        static int[] InsertShiftArray(int[] array, int value)
+
+        /// <summary>
+        /// This method takes in an array and a value, and returns a copy of that array with the value inserted at the middle index and subsequent values shifted toward the end of the array. Middle index is considered to be the current middle index + 1 for odd array lengths.
+        /// </summary>
+        /// <param name="array">The array to insert the value at the middle index of.</param>
+        /// <param name="value">The value to insert at the array's middle index.</param>
+        /// <returns>A new array, similar to the old, but with the value inserted at the middle index.</returns>
+        static int[] ShiftArray(int[] array, int value)
         {
-            int[] shiftedArray = new int[array.Length + 1]; // Creates the new, shifted array where we will be adding our value
-            int middleIndex = (array.Length+1) / 2;
-            for(int i=0; i < shiftedArray.Length; i++)
+            int[] shiftedArray = new int[array.Length + 1];
+            int middleIndex = (array.Length+1) / 2; // Index of the middle of the array. Uses integer division truncation to give correct index for an array whether it is even or odd.
+            for(int i=0; i < shiftedArray.Length; i++) // Fill in the values for the shifted array, depending on where each index is in relation to the middle index
             {
                 if (i == middleIndex)
                 {
---
+++ challenges/ArrayShift/ArrayShift/ArrayShiftTests/UnitTest1.cs
@@ -0,0 +1,46 @@
+using System;
+using Xunit;
+using ArrayShift;
+
+namespace ArrayShiftTests
+{
+    public class UnitTests
+    {
+        /*[Fact]
+        public void TestCanShiftArrayHandleEvenLengthArray()
+        {
+            int[] shiftedArray = ;
+            Assert.True(Program.ShiftArray({ 2,4,6,8}, 5) == [2, 4, 5, 6, 8}));
+        }
+        
+        [Fact]
+        public void TestCanShiftArrayHandleOddLengthArray()
+        {
+
+        }
+
+        [Fact]
+        public void TestCanShiftArrayHandleEmptyArray()
+        {
+
+        }
+
+        [Fact]
+        public void TestCanShiftArrayHandleLengthOneArray()
+        {
+
+        }
+
+        [Fact]
+        public void TestCanShiftArrayHandleNonIntegerArray()
+        {
+
+        }
+        
+        [Fact]
+        public void TestCanShiftArrayHandleNonIntegerValue()
+        {
+
+        }*/
+    }
+}
