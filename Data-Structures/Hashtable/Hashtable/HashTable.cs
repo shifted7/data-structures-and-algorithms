dfd9aa347dbd8fe8cae0ca8e1542ad02379d6bc0
Andrew <shifted6@gmail.com>
2020-04-30 08:56:43 -0700

    Hashtable methods added and tested.

--- Data-Structures/Hashtable/Hashtable/HashTable.cs
+++ Data-Structures/Hashtable/Hashtable/HashTable.cs
@@ -7,18 +7,59 @@ namespace Hashtable
     public class HashTable
     {
         public int Size { get; }
-        public object[] Contents { get; set; }
+        public LinkedList<KeyValuePair<string, string>>[] Contents { get; set; }
         public HashTable(int size)
         {
-            Contents = new object[size];
-            for(int i=0; i<size; i++)
-            {
-                LinkedList<Bucket> entries = new LinkedList<Bucket>();
-                Contents[i] = entries;
-            }
+            Contents = new LinkedList<KeyValuePair<string, string>>[size];
             Size = Contents.Length;
         }
 
-        //TODO: add methods for add, get, contains, and hash
+
+        public int Hash(string key)
+        {
+            int positionLoopCount = 8; // As part of the hash, multiply by a count that decreases from this number to 1, then loops. This prevents words with the same letters in different positions from having the same hash.
+            int sum = 0; // Sum of values for each character in the string after position factor
+            int prime = 31; // As part of hash, multiply by this prime number to minimize patterns after array size molulo operation
+            int asciiCharValue;
+            int positionFactor;
+            for(int i = 0; i<key.Length; i++)
+            {
+                asciiCharValue = (int)key[i];
+                positionFactor = positionLoopCount - (i % (positionLoopCount - 1)); // Counts down from 8 to 1 while traversing the string, then loops back up to 8
+                sum += asciiCharValue * positionFactor;
+            }
+            return sum * prime % Size;
+        }
+        
+        public int Add(string key, string value)
+        {
+            int hash = Hash(key);
+            KeyValuePair<string, string> keyValue = new KeyValuePair<string, string>(key, value);
+            var bucket = Contents[hash]; // The bucket we're planning to put our value
+            if (bucket == null) // If the bucket doesn't have a linkedlist yet, create one
+            {
+                bucket = new LinkedList<KeyValuePair<string, string>>();
+                Contents[hash] = bucket;
+            }
+            bucket.AddFirst(keyValue);
+            return hash;
+        }
+        
+        public string Get(string key)
+        {
+            int hash = Hash(key);
+            if (Contents[hash] == null)
+            {
+                return null;
+            }
+            foreach(var kvPair in Contents[hash])
+            {
+                if(kvPair.Key == key)
+                {
+                    return kvPair.Value;
+                }
+            }
+            return null;
+        }
     }
 }
--- Data-Structures/Hashtable/HashtableTests/UnitTest1.cs
+++ Data-Structures/Hashtable/HashtableTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Hashtable;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace HashtableTests
@@ -15,6 +16,38 @@ namespace HashtableTests
 
             //Assert
             Assert.Equal(128, testHashTable.Size);
+        }
+
+        [Fact]
+        public void TestCanHashKeys()
+        {
+            //Arrange
+            HashTable testHashTable = new HashTable(128);
+            //Act
+            string testKeyA = "Andrew";
+            int testHashA = testHashTable.Hash(testKeyA);
+            string testKeyB = "Werdna";
+            int testHashB = testHashTable.Hash(testKeyB);
+
+
+            //Assert
+            Assert.NotEqual(testHashA, testHashB);
+        }
+
+        [Fact]
+        public void TestCanAddStringValueToHashTable()
+        {
+            //Arrange
+            HashTable testHashTable = new HashTable(128);
+            //Act
+            string testKey = "Andrew";
+            string testValue = "Casper";
+            testHashTable.Add(testKey, testValue);
+            int expectedHash = testHashTable.Hash(testKey);
+            string result = testHashTable.Contents[expectedHash].First.Value.Value;
+
+            //Assert
+            Assert.Equal(testValue, result);
         }
     }
 }
